8ece8f0d2622dbcbfbb562e40bfebf18d258464d
boev <boev@rg.ru>
2024-12-24 16:32:26 +0200

    iter 7: fix pkg env

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 	"project_sem/pkg/db"
-	"project_sem/pkg/env"
+	"project_sem/pkg/envs"
 	"project_sem/server"
 
 	_ "github.com/lib/pq"
@@ -12,18 +12,18 @@ import (
 
 func init() {
 	// загружаем ENV
-	if err := env.Init(); err != nil {
+	if err := envs.Init(); err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
 	// подключаемся к DB
 	configDB := &db.ConfigDB{
-		User:     env.Config.UserDB,
-		Password: env.Config.PasswordDB,
-		Name:     env.Config.NameDB,
-		Host:     env.Config.HostDB,
-		Port:     env.Config.PortDB,
+		User:     envs.Config.UserDB,
+		Password: envs.Config.PasswordDB,
+		Name:     envs.Config.NameDB,
+		Host:     envs.Config.HostDB,
+		Port:     envs.Config.PortDB,
 	}
 
 	if err := db.Init(configDB); err != nil {
@@ -34,7 +34,7 @@ func init() {
 
 func main() {
 	// запускаем сервер
-	if err := server.Up(env.Config.Port); err != nil {
+	if err := server.Up(envs.Config.Port); err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
